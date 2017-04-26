d8163a1d4ec3590d9a411381ec39a0f4dd8645b6
Siddharth Atre <sidatre@gmail.com>
2017-04-26 16:32:07 -0700

    Added grading code and projct 1 code for comparison

--- Project 2/Project 2.cpp
+++ Project 2/Project 2.cpp
@@ -22,15 +22,25 @@ void project2() {
 	ground.MakeBox(5.0f, 0.1f, 5.0f);
 	scn.AddObject(ground);
 
+	MeshObject tallBox;
+	tallBox.MakeBox(1.0f, 4.0f, 1.0f);
+	//scn.AddObject(tallBox);
+
 	// Create dragon
 	MeshObject dragon;
 	dragon.LoadPLY("dragon.ply");
 	dragon.Smooth();
+
 	BoxTreeObject tree;
 	tree.Construct(dragon);
+	int count = 0;
+	tree.CountTriangles(count);
+	std::cerr << "Number of triangles in leaves of tree after construction: " << count << std::endl;
+	//tree.Construct(tallBox);
+
 	scn.AddObject(tree);
 
-	// Create instance
+	//// Create instance
 	InstanceObject inst(tree);
 	glm::mat4x4 mtx = glm::eulerAngleY(glm::pi<float>());
 	mtx[3] = glm::vec4(-0.05f, 0.0f, -0.1f, 1.0f);
@@ -60,9 +70,11 @@ void project2() {
 	Camera cam;
 	cam.BuildCamera(glm::vec3(-0.1f, 0.1f, 0.2f), glm::vec3(-0.05f, 0.12f, 0.0f),
 		glm::vec3(0, 1.0f, 0));
+	//cam.BuildCamera(glm::vec3(2.0f, 2.0f, 5.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0, 1, 0));
 	cam.SetFoV(40.0f);
 	cam.SetAspect(1.33f);
-	cam.SetResolution(800, 600);
+	cam.SetResolution(800, 600);
+
 	// Render image
 	cam.Render(scn);
 	cam.SaveBitmap("project2.bmp");
@@ -74,7 +86,67 @@ void project2() {
 #endif // _WIN32
 }
 
+void project1() {
+	// Create scene
+	Scene scn;
+	scn.SetSkyColor(Color(0.8f, 0.9f, 1.0f));
+
+	// Create boxes
+	MeshObject box1;
+	box1.MakeBox(5.0f, 0.1f, 5.0f);
+	scn.AddObject(box1);
+
+	MeshObject box2;
+	box2.MakeBox(1.0f, 1.0f, 1.0f);
+
+	InstanceObject inst1(box2);
+	glm::mat4 mtx = glm::rotate(glm::mat4(), 0.5f, glm::vec3(1, 0, 0));
+	mtx[3][1] = 1.0f;
+	inst1.SetMatrix(mtx);
+	scn.AddObject(inst1);
+
+	InstanceObject inst2(box2);
+	glm::mat4 mtx2 = glm::rotate(glm::mat4(), 1.0f, glm::vec3(0, 1, 0));
+	mtx2[3] = glm::vec4(-1, 0, 1, 1);
+	inst2.SetMatrix(mtx2);
+	scn.AddObject(inst2);
+
+	// Create lights
+	DirectLight sunlgt;
+	sunlgt.SetBaseColor(Color(1.0f, 1.0f, 0.9f));
+	sunlgt.SetIntensity(0.5f);
+	sunlgt.SetDirection(glm::vec3(-0.5f, -1.0f, -0.5f));
+	scn.AddLight(sunlgt);
+
+	PointLight redlgt;
+	redlgt.SetBaseColor(Color(1.0f, 0.2f, 0.2f));
+	redlgt.SetIntensity(2.0f);
+	redlgt.SetPosition(glm::vec3(2.0f, 2.0f, 0.0f));
+	scn.AddLight(redlgt);
+
+	// Create camera
+	Camera cam;
+	cam.BuildCamera(glm::vec3(2.0f, 2.0f, 5.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0, 1, 0));
+	cam.SetResolution(800, 600);
+	cam.SetFoV(40.0f);
+	cam.SetAspect(1.33f);
+
+	// Render image
+	cam.Render(scn, false);
+	cam.SaveBitmap("project1.bmp");
+
+#ifdef _WIN32
+	std::system("project1.bmp");
+#else
+	std::system("open project1.bmp");
+#endif // _WIN32
+
+	//Open the image
+	std::system("open project1.bmp");
+}
+
 int main() {
 	project2();
+	//project1();
 	return 0;
 }
