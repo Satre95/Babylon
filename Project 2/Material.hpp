e81d285d60842d4ffdeb4d7602746da72af87c1e
Siddharth Atre <sidatre@gmail.com>
2017-05-11 00:01:23 -0700

    Changed output var to no longer be constant

--- Project 2/Material.hpp
+++ Project 2/Material.hpp
@@ -11,7 +11,7 @@
 
 class Material {
 public:
-	virtual void ComputeReflectance(Color &col, const glm::vec3 &in, const glm::vec3 &out, const Intersection &hit) = 0;
+	virtual void ComputeReflectance(Color &col, const glm::vec3 &in, glm::vec3 &out, const Intersection &hit) = 0;
 	virtual void GenerateSample(const Intersection & isect, const glm::vec3 & inDir, glm::vec3 & outDir, Color & outColor) = 0;
 };
 typedef std::shared_ptr<Material> MaterialPtr;
