f387afff7c2ff35a2b0442e7a1f19e9e8331061e
Siddharth Atre <sidatre@gmail.com>
2017-05-11 00:01:01 -0700

    Replaced my random fn with glm's linear rand fn

--- Project 2/LambertMaterial.hpp
+++ Project 2/LambertMaterial.hpp
@@ -6,12 +6,13 @@
 
 #include "Material.hpp"
 #include <glm/gtc/constants.hpp>
+#include <glm/gtc/random.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////
 
 class LambertMaterial :public Material {
 public:
-	void ComputeReflectance(Color &col, const glm::vec3 &in, const glm::vec3 &out, const Intersection &hit);
+	void ComputeReflectance(Color &col, const glm::vec3 &in, glm::vec3 &out, const Intersection &hit);
 
 	void GenerateSample(const Intersection & isect, const glm::vec3 & inDir, glm::vec3 & outDir, Color & outColor);
 
