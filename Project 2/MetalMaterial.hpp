79bee46452114b50c54575f3b1209d4f34f15cd0
Siddharth Atre <sidatre@gmail.com>
2017-05-11 00:01:45 -0700

    Implemented fns, not fully correct

--- Project 2/MetalMaterial.hpp
+++ Project 2/MetalMaterial.hpp
@@ -6,7 +6,7 @@
 class MetalMaterial : public Material
 {
 public:
-	void ComputeReflectance(Color &col, const glm::vec3 &in, const glm::vec3 &out, const Intersection &hit);
+	void ComputeReflectance(Color &col, const glm::vec3 &in, glm::vec3 &out, const Intersection &hit);
 	void GenerateSample(const Intersection & isect, const glm::vec3 & inDir, glm::vec3 & outDir, Color & outColor);
 
 	void SetColor(Color & c) { matColor = c; }
