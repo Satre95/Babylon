964a8ee36393d2e2d6933392d5627568831b3657
Siddharth Atre <sidatre@gmail.com>
2017-05-10 17:06:14 -0700

    Calculate random sample of output dir

--- Project 2/LambertMaterial.cpp
+++ Project 2/LambertMaterial.cpp
@@ -1,9 +1,17 @@
 #include "LambertMaterial.hpp"
-
+#include "Utilities.hpp"
 void LambertMaterial::ComputeReflectance(Color &col, const glm::vec3 &in, const glm::vec3 &out, const Intersection &hit)
 {
 	col = DiffuseColor;
 	col.Scale(1.f / glm::pi<float>());
 }
 void LambertMaterial::GenerateSample(const Intersection & isect, const glm::vec3 & inDir, glm::vec3 & outDir, Color & outColor) {
+	//Randomly sample an output dir over the cosing weighted hemisphere.
+	float s = Utilities::randomFloatInRange(0.f, 1.f);
+	float t = Utilities::randomFloatInRange(0.f, 1.f);
+	float u = 2.f * glm::pi<float>() * s;
+	float v = pow(1 - t, 0.5f);
+	outDir.x = v * cos(u);
+	outDir.y = pow(t, 0.5f);
+	outDir.z = v * sin(u);
 }
