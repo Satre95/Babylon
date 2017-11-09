ac92e6c6c02a4cf2002751de37fa48912a3aaa71
Siddharth Atre <sidatre@gmail.com>
2017-11-08 17:11:17 -0800

    Added virtual destructor

--- include/Material.hpp
+++ include/Material.hpp
@@ -11,6 +11,7 @@
 
 class Material {
 public:
+	virtual ~Material() {};
 	virtual void ComputeReflectance(Color &col, const glm::vec3 &in, glm::vec3 &out, const Intersection &hit) = 0;
 	virtual void GenerateSample(const Intersection & isect, const glm::vec3 & inDir, glm::vec3 & outDir, Color & outColor) = 0;
 };
