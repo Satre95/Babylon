abe218566d630162a986bbaa8b2f8be3eafd4577
Siddharth Atre <sidatre@gmail.com>
2017-11-23 17:29:24 -0800

    Disabled MACOSX_BUNDLE from CMake configs

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -50,12 +50,12 @@ int main(int argc, char* argv[]) {
 
 	//Make a camera
 	Camera cam;
-	cam.BuildCamera( glm::vec3(0, 0.5f, 10.f), glm::vec3(0), glm::vec3(0, 1.0f, 0));
-	cam.SetResolution(400, 400);
+	cam.BuildCamera( glm::vec3(0, 0.5f, 1.f), glm::vec3(0), glm::vec3(0, 1.0f, 0));
+	cam.SetResolution(1000, 1000);
 	cam.SetFoV(45.f);
 	cam.SetFocus(1.f);
-	cam.SetfStop(2000.f);
-	cam.SetSuperSample(2, 2);
+	cam.SetfStop(20000.f);
+	cam.SetSuperSample(1, 1);
 	cam.SetJitter(true);
 	cam.SetShirley(true);
 	cam.SetMaxPathLength(3);
